8a3a0f2876ac9558daefa4e2c4bc37ff2e943f7a
Chris Cowan <chriscowan@plus3network.com>
2014-07-10 14:26:50 -0700

    Breaking things out and completing initial functionality

--- runner.js
+++ runner.js
@@ -2,6 +2,8 @@ var releases = require('./lib/releases');
 var loader = require('./index');
 var Node = require('./lib/node');
 var path = require('path');
+var clc = require('cli-color');
+var moment = require('moment');
 
 var options = {
   version: '~1.2.0',
@@ -9,7 +11,7 @@ var options = {
   plugins: ['elasticsearch/marvel/latest'],
   purge: true, // Purge the data directory
   fresh: false, // Download a fresh copy
-  nodes: 1,
+  nodes: 2,
   config: {
     cluster: {
       name: "My Test Cluster"
@@ -17,28 +19,39 @@ var options = {
   }
 };
  
- // var cluster = loader.createCluster(options);
- // cluster.start()
- //   .then(function (args) {
- //     console.log('done', args);
- //   })
- //   .catch(function (err) {
- //     console.log('Oops', err.stack);
- //   });
+var cluster = loader.createCluster(options);
 
-var root = path.join(options.directory, '1.2.1');
-var node = new Node({
-  root: root,
-  version: '1.2.1',
-  config: options.config
+var levels = {
+  INFO: clc.green,
+  DEBUG: clc.cyan,
+  WARN: clc.yellow,
+  FATAL: clc.magentaBright,
+  ERROR: clc.red
+};
+
+cluster.on('log', function (log) {
+  var level = levels[log.level] || function (msg) { return msg; };
+  var message = clc.blackBright(moment(log.timestamp).format('lll'));
+  message += ' '+level(log.level);
+  message += ' ' + clc.yellow(log.type);
+  if (log.node) message += ' ' + clc.magenta(log.node) + ': ';
+  message += log.message;
+  console.log(message);
 });
 
-node.on('log', function (msg) {
-  // console.log(msg);
-});  
-
-node.start().then(function () {
-  console.log('[Elasticsearch '+node.version+']', '"'+node.name+'"', 'started on', 'http://'+node.address+':'+node.port);
+cluster.download().then(function () {
+ return cluster.installPlugins();
+}).then(function () {
+ return cluster.start(); 
+}).then(function () {
+  process.on('SIGINT', function () {
+    cluster.shutdown().then(function () {
+      console.log(clc.white.bgGreen("Bye Bye!"));
+      process.exit();
+    });
+  });
+  process.stdin.read();
 }).catch(function (err) {
-  console.log('Oops', err.stack);
+ console.log('Oops', err.stack);
 });
+
