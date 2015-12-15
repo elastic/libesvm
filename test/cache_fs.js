3efa99eb7e524b05cc7c8e39f024a2facd0e85c2
Joe Fleming <joe.fleming@gmail.com>
2015-12-14 18:17:09 -0700

    wrap base cache tests, use temp
    remove the previous tmpfile helper

--- test/cache_fs.js
+++ test/cache_fs.js
@@ -1,34 +1,18 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var cache = require('../lib/cache');
-var crypto = require('crypto');
-
-/**
- * A utility to create a temp file name
- * @param {string} prefix The prefix for the cache file
- * @param {function} cb The node style callback
- * @returns {null}
- */
-var tmpfile = function (prefix, cb) {
-  prefix = prefix && prefix + '-' || '';
-  crypto.randomBytes(32, function (err, buf) {
-    if (err) return cb(err);
-    var filename = crypto.createHash('sha1')
-      .update(buf)
-      .digest('hex');
-    cb(null, '/tmp/' + prefix + filename);
-  });
-};
+var temp = require('temp').track();
 
 describe('Cache', function() {
-  describe('File opperations', function() {
+  describe('File operations', function() {
+    after(function (done) {
+      temp.cleanup(done);
+    });
 
     it('should return an empty object when fetch is called and the cache is new', function (done) {
-      tmpfile('empty-test', function (err, filename) {
-        var source = cache.source;
-        cache.source = filename;
+      temp.open('empty-test', function (err, filename) {
+        cache.source = filename.path;
         cache.fetch().then(function (data) {
-          cache.source = source;
           expect(data).to.be.empty;
           done();
         })
@@ -37,21 +21,21 @@ describe('Cache', function() {
     });
 
     it('should save a new value', function (done) {
-      tmpfile('test', function (err, filename) {
-        var source = cache.source;
-        cache.source = filename;
+      temp.open('value-test', function (err, filename) {
+        cache.source = filename.path;
         cache.set('myTest', 'test');
         cache.save(function (err) {
-          cache.source = source;
           if (err) return done(err);
-            fs.readFile(filename, function (err, buf) {
-              if (err) return done(err);    
-                var data = JSON.parse(buf.toString('utf8'));
-              expect(data).to.have.property('myTest', 'test');
-              fs.unlink(filename, done);
-            });
-        });
 
+          fs.readFile(filename.path, { encoding: 'utf8' }, function (err, contents) {
+            if (err) return done(err);
+
+            var data = JSON.parse(contents);
+            expect(data).to.have.property('myTest', 'test');
+            // console.log(contents, data);
+            done();
+          });
+        });
       });
     });
   });
